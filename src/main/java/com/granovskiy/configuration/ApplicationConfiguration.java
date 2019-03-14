0bccb324a14460ce497c42047172896287a752e9
Sviatoslav <s.granovskiy@gmail.com>
2019-03-14 08:47:51 +0200

    spring installation

---
+++ src/main/java/com/granovskiy/configuration/ApplicationConfiguration.java
@@ -0,0 +1,21 @@
+package com.granovskiy.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.granovskiy")
+public class ApplicationConfiguration {
+
+    @Bean
+    public InternalResourceViewResolver ViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
